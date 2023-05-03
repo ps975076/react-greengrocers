3eec782f95313f41404434bc7bcbea96e2a2c75c
Lewis <ljoncb@gmail.com>
2023-05-03 17:26:47 +0100

    using createRoot

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
 
-import App from "./App";
+import App from './App'
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  rootElement
-);
+  </StrictMode>
+)
