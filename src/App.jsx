0dec34d210d1e4d29d7983b400bb79468834d4aa
Priya <Ps975076@gmail.com>
2023-10-31 12:05:40 +0000

    rendering wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import './styles/reset.css'
-import './styles/index.css'
+// A user can view a selection of items in the store
+// From the store, a user can add an item to their cart
+// From the cart, a user can view and adjust the number of items in their cart
+// If an item's quantity equals zero it is removed from the cart
+// A user can view the current total in their cart
 
-import initialStoreItems from './store-items'
+// Create all the components you need, using the templates as a reference
+// Create all the pieces of the state you need, using the example provided
+// Add all the event listeners you need to make the page reactive
+
+import "./styles/reset.css";
+import "./styles/index.css";
+
+import initialStoreItems from "./store-items";
+import { useState } from "react";
 
 /*
  Here's what a store item should look like
@@ -14,10 +25,12 @@ import initialStoreItems from './store-items'
  What should a cart item look like? 🤔
  */
 
-console.log(initialStoreItems)
+console.log(initialStoreItems);
 
 export default function App() {
-  // Setup state here...
+  // 1. Setup state here...
+  const [storeItems, setStoreItems] = useState(initialStoreItems);
+  const [cartItems, setCartItems] = useState();
 
   return (
     <>
@@ -25,6 +38,19 @@ export default function App() {
         <h1>Greengrocers</h1>
         <ul className="item-list store--item-list">
           {/* Write some code here... */}
+          {/* 2.  Copied the template store-item here */}
+          {/* 3. Map through all the items (Render) */}
+          {storeItems.map((storeItems) => {
+            return (
+              <li key={storeItems.id}>
+                <div className="store--item-icon">
+                  <img src="/assets/icons/001-beetroot.svg" alt="beetroot" />
+                </div>
+                {/* 4. Add an onClick here so when the "Add to cart" button is clicked the cart items will increase in quantity */}
+                <button>Add to cart</button>
+              </li>
+            );
+          })}
         </ul>
       </header>
       <main id="cart">
@@ -32,6 +58,20 @@ export default function App() {
         <div className="cart--item-list-container">
           <ul className="item-list cart--item-list">
             {/* Write some code here... */}
+            {/* 5. Copied the template cart-items here */}
+            {/* 6. Map through all the cart-items ??*/}
+            <li>
+              <img
+                className="cart--item-icon"
+                src="assets/icons/001-beetroot.svg"
+                alt="beetroot"
+              />
+              <p>beetroot</p>
+              <button className="quantity-btn remove-btn center">-</button>
+              <span className="quantity-text center">1</span>
+              <button className="quantity-btn add-btn center">+</button>
+            </li>
+            {/* Li for cart-items end here */}
           </ul>
         </div>
         <div className="total-section">
@@ -57,5 +97,5 @@ export default function App() {
         </a>
       </div>
     </>
-  )
+  );
 }
